b6af929e5aab38d5babe66875a7347afe111beab
NoRePercussions <66567994+NoRePercussions@users.noreply.github.com>
2021-10-27 11:03:06 -0400

    Messenger unittests

--- test/test_chats.py
+++ test/test_chats.py
@@ -54,3 +54,48 @@ class DiscordChatTest(unittest.TestCase):
         chatB.load(self.raw_data_path + self.server_message_path)
 
         self.assertEqual(chatA, chatB)
+
+
+
+class MessengerChatTest(unittest.TestCase):
+    baseline_path = "test/test_data/messenger/baseline/"
+    raw_data_path = "test/test_data/messenger/messages/inbox/"
+    end = "/message_1.json"
+    direct_message_path = "directmessage_78o3u1q7"
+    group_message_path = "groupmessage_99hdkg23"
+
+    def test_direct_message_import(self):
+        group = chatanalytics.MessengerChat()
+        group.load(self.raw_data_path + self.direct_message_path + self.end)
+        with open(self.baseline_path + self.direct_message_path + ".p", "rb") as f:
+            baseline = pickle.load(f)
+            self.assertEqual(group, baseline)
+
+    def test_group_message_import(self):
+        group = chatanalytics.MessengerChat()
+        group.load(self.raw_data_path + self.group_message_path + self.end)
+        with open(self.baseline_path + self.group_message_path + ".p", "rb") as f:
+            baseline = pickle.load(f)
+            self.assertEqual(group, baseline)
+
+    def test_batch_import(self):
+        group = chatanalytics.MessengerChat()
+        group.batch_load(self.raw_data_path)
+        with open(self.baseline_path + "chat_batch.p", "rb") as f:
+            baseline = pickle.load(f)
+            self.assertEqual(group, baseline)
+
+    def test_repeated_import(self):
+        chat = chatanalytics.MessengerChat()
+        chat.load(self.raw_data_path + self.direct_message_path + self.end)
+        chat.load(self.raw_data_path + self.group_message_path + self.end)
+
+    def test_batch_import_equals_import(self):
+        chatA = chatanalytics.MessengerChat()
+        chatA.batch_load(self.raw_data_path)
+
+        chatB = chatanalytics.MessengerChat()
+        chatB.load(self.raw_data_path + self.direct_message_path + self.end)
+        chatB.load(self.raw_data_path + self.group_message_path + self.end)
+
+        self.assertEqual(chatA, chatB)
--- test/utils_for_test_cases.py
+++ test/utils_for_test_cases.py
@@ -4,8 +4,16 @@
 from random import randint
 
 
-def gen_id(length):
+def gen_numerical_id(length):
     s = ""
     for i in range(length):
         s += str(randint(0, 9))
+    return s
+
+
+def gen_messenger_id(length):
+    s = ""
+    chars = "abcdefghijklmnopqrstuvwxyz0123456789-_"
+    for i in range(length):
+        s += chars[randint(0, len(chars))]
     return s
